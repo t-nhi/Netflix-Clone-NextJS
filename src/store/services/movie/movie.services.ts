556897b07f8f82da1246b5f57c9d83ba4d7a52ed
Nguyen Dang Truong <ndtrg281@gmail.com>
2025-11-14 20:11:50 +0700

    feat: add movie management functionalities including search, create, update, and delete operations

--- src/store/services/movie/movie.services.ts
+++ src/store/services/movie/movie.services.ts
@@ -4,6 +4,10 @@ import { GetMovieListQueryParamsType, GetMovieListResType } from '@/types/dtos/m
 import { HttpMethod } from '@/constants/http.enum'
 import { GetMovieByIdParamsType, GetMovieByIdResType } from '@/types/dtos/movie/getMovieById.dto'
 import { GetAllMovieResType } from '@/types/dtos/movie/getAllMovie.dto'
+import { CreateMovieBodyType, CreateMovieResType } from '@/types/dtos/movie/createMovie.dto'
+import { UpdateMovieBodyType, UpdateMovieParamsType, UpdateMovieResType } from '@/types/dtos/movie/updateMovie.dto'
+import { DeleteMovieParamsType, DeleteMovieResType } from '@/types/dtos/movie/deleteMovie.dto'
+import { SearchMovieQueryType, SearchMovieResType } from '@/types/dtos/movie/searchMovie.dto'
 
 export const movieApi = createApi({
     reducerPath: 'movieApi',
@@ -19,7 +23,7 @@ export const movieApi = createApi({
                 url: '/movies/all',
                 method: HttpMethod.GET
             }),
-            providesTags: (result, error, arg) => {
+            providesTags: (result) => {
                 const tags = result
                     ? result.data.map((movie) => ({
                           type: 'Movie' as const,
@@ -76,8 +80,85 @@ export const movieApi = createApi({
                     return current_page - 1
                 }
             }
+        }),
+        searchMovies: builder.infiniteQuery<SearchMovieResType, { query: SearchMovieQueryType }, number>({
+            query: ({ pageParam, queryArg: { query } }) => {
+                const queryString = new URLSearchParams({
+                    ...query,
+                    page: pageParam ? pageParam.toString() : '1'
+                }).toString()
+
+                return {
+                    url: `/movies/search?${queryString}`,
+                    method: HttpMethod.GET
+                }
+            },
+            providesTags: (result) =>
+                result
+                    ? [
+                          ...result.pages
+                              .flatMap((page) => page.data)
+                              .map((movie) => ({
+                                  type: 'Movie' as const,
+                                  id: movie.id
+                              })),
+                          { type: 'Movie', id: 'LIST' }
+                      ]
+                    : [{ type: 'Movie', id: 'LIST' }],
+            infiniteQueryOptions: {
+                initialPageParam: 1,
+                maxPages: 5,
+                getNextPageParam: ({ meta }) => {
+                    if (!meta) return undefined
+                    const { total_pages, current_page } = meta
+                    if (current_page >= total_pages) return undefined
+                    return current_page + 1
+                },
+                getPreviousPageParam: ({ meta }) => {
+                    if (!meta) return undefined
+                    const { current_page } = meta
+                    if (current_page <= 1) return undefined
+                    return current_page - 1
+                }
+            }
+        }),
+        createMovie: builder.mutation<CreateMovieResType, CreateMovieBodyType>({
+            query: (body) => ({
+                url: '/movies',
+                method: HttpMethod.POST,
+                body
+            }),
+            invalidatesTags: [{ type: 'Movie', id: 'LIST' }]
+        }),
+        updateMovie: builder.mutation<UpdateMovieResType, { params: UpdateMovieParamsType; body: UpdateMovieBodyType }>(
+            {
+                query: ({ params, body }) => ({
+                    url: `/movies/${params.movieId}`,
+                    method: HttpMethod.PUT,
+                    body
+                }),
+                invalidatesTags: (result, error, arg) => [{ type: 'Movie', id: arg.params.movieId }]
+            }
+        ),
+        deleteMovie: builder.mutation<DeleteMovieResType, { params: DeleteMovieParamsType }>({
+            query: ({ params }) => ({
+                url: `/movies/${params.movieId}`,
+                method: HttpMethod.DELETE
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'Movie', id: arg.params.movieId },
+                { type: 'Movie', id: 'LIST' }
+            ]
         })
     })
 })
 
-export const { useGetAllMoviesQuery, useGetMovieByIdQuery, useGetMoviesInfiniteQuery } = movieApi
+export const {
+    useGetAllMoviesQuery,
+    useGetMovieByIdQuery,
+    useGetMoviesInfiniteQuery,
+    useSearchMoviesInfiniteQuery,
+    useCreateMovieMutation,
+    useUpdateMovieMutation,
+    useDeleteMovieMutation
+} = movieApi
